bdc24d046e27fde9f4abb3bf7d40d0bde4b1d7c2
mtogneri <mtogneri@openwt.com>
2018-03-13 00:16:40 +0100

    Using views as interactive elements

--- app/src/main/java/com/mauriciotogneri/escaperoom/scenes/SecondScene.java
+++ app/src/main/java/com/mauriciotogneri/escaperoom/scenes/SecondScene.java
@@ -8,6 +8,16 @@ public class SecondScene extends BaseFragment
     protected void initialize()
     {
         view.findViewById(R.id.button_back).setOnClickListener(view1 -> openFirstScene());
+
+        view.findViewById(R.id.button_1).setOnClickListener(view1 -> {});
+        view.findViewById(R.id.button_2).setOnClickListener(view1 -> {});
+        view.findViewById(R.id.button_3).setOnClickListener(view1 -> {});
+        view.findViewById(R.id.button_4).setOnClickListener(view1 -> {});
+        view.findViewById(R.id.button_5).setOnClickListener(view1 -> {});
+        view.findViewById(R.id.button_6).setOnClickListener(view1 -> {});
+        view.findViewById(R.id.button_7).setOnClickListener(view1 -> {});
+        view.findViewById(R.id.button_8).setOnClickListener(view1 -> {});
+        view.findViewById(R.id.button_9).setOnClickListener(view1 -> {});
     }
 
     @Override
