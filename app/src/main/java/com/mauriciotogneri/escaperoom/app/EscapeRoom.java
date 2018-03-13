a78ec40aaf7bb6ba4d11ecc80564c0aae05a36cf
mtogneri <mtogneri@openwt.com>
2018-03-13 21:49:23 +0100

    Playing sounds

--- app/src/main/java/com/mauriciotogneri/escaperoom/app/EscapeRoom.java
+++ app/src/main/java/com/mauriciotogneri/escaperoom/app/EscapeRoom.java
@@ -2,11 +2,15 @@ package com.mauriciotogneri.escaperoom.app;
 
 import android.app.Application;
 
+import com.mauriciotogneri.escaperoom.audio.AudioManager;
+
 public class EscapeRoom extends Application
 {
     @Override
     public void onCreate()
     {
         super.onCreate();
+
+        AudioManager.initialize(this);
     }
 }
--- app/src/main/java/com/mauriciotogneri/escaperoom/scenes/Scene2.java
+++ app/src/main/java/com/mauriciotogneri/escaperoom/scenes/Scene2.java
@@ -1,6 +1,7 @@
 package com.mauriciotogneri.escaperoom.scenes;
 
 import com.mauriciotogneri.escaperoom.R;
+import com.mauriciotogneri.escaperoom.audio.AudioManager;
 
 public class Scene2 extends BaseFragment
 {
@@ -9,15 +10,20 @@ public class Scene2 extends BaseFragment
     {
         view.findViewById(R.id.button_back).setOnClickListener(view1 -> openFirstScene());
 
-        view.findViewById(R.id.button_1).setOnClickListener(view1 -> {});
-        view.findViewById(R.id.button_2).setOnClickListener(view1 -> {});
-        view.findViewById(R.id.button_3).setOnClickListener(view1 -> {});
-        view.findViewById(R.id.button_4).setOnClickListener(view1 -> {});
-        view.findViewById(R.id.button_5).setOnClickListener(view1 -> {});
-        view.findViewById(R.id.button_6).setOnClickListener(view1 -> {});
-        view.findViewById(R.id.button_7).setOnClickListener(view1 -> {});
-        view.findViewById(R.id.button_8).setOnClickListener(view1 -> {});
-        view.findViewById(R.id.button_9).setOnClickListener(view1 -> {});
+        view.findViewById(R.id.button_1).setOnClickListener(view1 -> onButtonClick(1));
+        view.findViewById(R.id.button_2).setOnClickListener(view1 -> onButtonClick(2));
+        view.findViewById(R.id.button_3).setOnClickListener(view1 -> onButtonClick(3));
+        view.findViewById(R.id.button_4).setOnClickListener(view1 -> onButtonClick(4));
+        view.findViewById(R.id.button_5).setOnClickListener(view1 -> onButtonClick(5));
+        view.findViewById(R.id.button_6).setOnClickListener(view1 -> onButtonClick(6));
+        view.findViewById(R.id.button_7).setOnClickListener(view1 -> onButtonClick(7));
+        view.findViewById(R.id.button_8).setOnClickListener(view1 -> onButtonClick(8));
+        view.findViewById(R.id.button_9).setOnClickListener(view1 -> onButtonClick(9));
+    }
+
+    private void onButtonClick(int id)
+    {
+        AudioManager.getInstance().playSound("scene2/button.ogg");
     }
 
     @Override
