02c7f48ff2a94fcc3895ca3e9ca7cdb34fad357b
mtogneri <mtogneri@openwt.com>
2018-03-15 21:44:22 +0100

    Code cleanup

--- app/src/main/java/com/mauriciotogneri/escaperoom/scenes/scene1/Scene1b.java
+++ app/src/main/java/com/mauriciotogneri/escaperoom/scenes/scene1/Scene1b.java
@@ -17,34 +17,34 @@ public class Scene1b extends BaseFragment<StateScene1>
     {
         background(R.drawable.scene1b_background);
 
-        addObject(InteractiveObject.fromWidget(getContext(), R.layout.widget_button_back_left), 2, 5, this::openScene1a);
+        addObject(InteractiveObject.fromWidget(getContext(), R.layout.widget_button_back_left), 2, 4, this::openScene1a);
 
         InteractiveObject b1 = InteractiveObject.fromWidget(getContext(), R.layout.widget_scene1b_button_pad, "1");
-        addObject(b1, 31, 20, () -> onButtonClick(1));
+        addObject(b1, 31, 18, () -> onButtonClick(1));
 
         InteractiveObject b2 = InteractiveObject.fromWidget(getContext(), R.layout.widget_scene1b_button_pad, "2");
-        addObject(b2, 44, 20, () -> onButtonClick(2));
+        addObject(b2, 44, 18, () -> onButtonClick(2));
 
         InteractiveObject b3 = InteractiveObject.fromWidget(getContext(), R.layout.widget_scene1b_button_pad, "3");
-        addObject(b3, 57, 20, () -> onButtonClick(3));
+        addObject(b3, 57, 18, () -> onButtonClick(3));
 
         InteractiveObject b4 = InteractiveObject.fromWidget(getContext(), R.layout.widget_scene1b_button_pad, "4");
-        addObject(b4, 31, 43, () -> onButtonClick(4));
+        addObject(b4, 31, 39, () -> onButtonClick(4));
 
         InteractiveObject b5 = InteractiveObject.fromWidget(getContext(), R.layout.widget_scene1b_button_pad, "5");
-        addObject(b5, 44, 43, () -> onButtonClick(5));
+        addObject(b5, 44, 39, () -> onButtonClick(5));
 
         InteractiveObject b6 = InteractiveObject.fromWidget(getContext(), R.layout.widget_scene1b_button_pad, "6");
-        addObject(b6, 57, 43, () -> onButtonClick(6));
+        addObject(b6, 57, 39, () -> onButtonClick(6));
 
         InteractiveObject b7 = InteractiveObject.fromWidget(getContext(), R.layout.widget_scene1b_button_pad, "7");
-        addObject(b7, 31, 66, () -> onButtonClick(7));
+        addObject(b7, 31, 60, () -> onButtonClick(7));
 
         InteractiveObject b8 = InteractiveObject.fromWidget(getContext(), R.layout.widget_scene1b_button_pad, "8");
-        addObject(b8, 44, 66, () -> onButtonClick(8));
+        addObject(b8, 44, 60, () -> onButtonClick(8));
 
         InteractiveObject b9 = InteractiveObject.fromWidget(getContext(), R.layout.widget_scene1b_button_pad, "9");
-        addObject(b9, 57, 66, () -> onButtonClick(9));
+        addObject(b9, 57, 60, () -> onButtonClick(9));
 
         dark = InteractiveObject.fromWidget(getContext(), R.layout.widget_scene1_dark);
         addObject(dark, 0, 0);
