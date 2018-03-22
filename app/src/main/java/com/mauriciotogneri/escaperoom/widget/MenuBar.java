3237d3a49180d95d16e184e6227adefb2077fc5b
mtogneri <mtogneri@openwt.com>
2018-03-22 23:34:25 +0100

    Improved menu bar

--- app/src/main/java/com/mauriciotogneri/escaperoom/widget/MenuBar.java
+++ app/src/main/java/com/mauriciotogneri/escaperoom/widget/MenuBar.java
@@ -3,6 +3,7 @@ package com.mauriciotogneri.escaperoom.widget;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build.VERSION_CODES;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -14,8 +15,13 @@ import android.widget.RelativeLayout;
 import com.mauriciotogneri.escaperoom.R;
 import com.mauriciotogneri.escaperoom.state.BaseSceneState;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MenuBar extends LinearLayout
 {
+    private final List<ImageView> items = new ArrayList<>();
+
     public MenuBar(Context context)
     {
         super(context);
@@ -48,13 +54,36 @@ public class MenuBar extends LinearLayout
         LinearLayout container = findViewById(R.id.items);
         container.removeAllViews();
 
+        items.clear();
+
         if (state.hasKey())
         {
-            ImageView item = new ImageView(getContext());
-            item.setImageResource(R.drawable.scene1c_key);
+            ImageView item = item(R.drawable.scene1c_key);
+            item.setOnClickListener(view -> selectItem(item));
 
             container.addView(item);
         }
+    }
+
+    private ImageView item(@DrawableRes int resId)
+    {
+        ImageView item = (ImageView) ImageView.inflate(getContext(), R.layout.view_menu_item, null);
+        item.setImageResource(resId);
+
+        return item;
+    }
+
+    private void selectItem(ImageView selectedItem)
+    {
+        for (ImageView item : items)
+        {
+            if (item != selectedItem)
+            {
+                item.setSelected(false);
+            }
+        }
+
+        selectedItem.setSelected(!selectedItem.isSelected());
     }
 
     public static MenuBar create(ViewGroup root)
